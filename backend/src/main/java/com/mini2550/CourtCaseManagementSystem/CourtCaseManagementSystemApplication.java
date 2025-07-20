5ab0445818a5a38ca65cb7d7d973a674916d4ef0
Erfan Habeeb <137034782+erfanhabeeb666@users.noreply.github.com>
2025-07-21 03:37:17 +0530

    intial backend bootstrap

---
+++ backend/src/main/java/com/mini2550/CourtCaseManagementSystem/CourtCaseManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.mini2550.CourtCaseManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourtCaseManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourtCaseManagementSystemApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mini2550/CourtCaseManagementSystem/CourtCaseManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mini2550.CourtCaseManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourtCaseManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
